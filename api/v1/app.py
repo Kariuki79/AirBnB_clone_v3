308793d0a9dee47d5cedcfeacd5f4f96b5a62818
willykar <wilsonnjenga799@gmail>
2024-04-29 12:09:48 -0700

    update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.errorhandler(404)
@@ -22,7 +22,7 @@ def teardown(self):
     storage.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
     app.run(host=host, port=int(port), threaded=True)
