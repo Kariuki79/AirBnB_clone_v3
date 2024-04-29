7718915dcf0bbd8003e588b3c11f738476ef9e7c
James gitere <sasmuypwa2019.com>
2024-04-29 23:24:50 +0300

    updated

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -24,7 +24,8 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_amenity(amenity_id):
     """Deletes a Amenity object by its ID"""
     amenity = storage.get(Amenity, amenity_id)
@@ -51,7 +52,8 @@ def create_amenity():
     return make_response(new_amenity.to_dict(), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     """Updates a Amenity object by its ID"""
     amenity = storage.get(Amenity, amenity_id)
