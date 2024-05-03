ac064c8e8569f785600ea260fba3bd918edaa5e5
Kariuki79 <kariukimwangi75@gmail.com>
2024-05-03 12:24:39 -0700

    db_storage method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class DBStorage
+Thus module Contains the class DBStorage
 """
 
 import models
@@ -74,3 +74,31 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ Retrieves an object
+        Attributes:
+            cls: class
+            id: string representing the object ID
+        Return: the object based on the class
+                and its ID, or None if not found
+        """
+        if cls in classes.values() and id and isinstance(id, str):
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """ Counts the number of instances
+        Attributes:
+            cls: class (optional)
+        Return: The number of objects in storage matching the
+        given class. If no class is passed, returns the count
+        of all objects in storage
+        """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
